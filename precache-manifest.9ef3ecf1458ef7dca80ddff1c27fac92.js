b797d36cfa880e9a2514a7e452cc8899bf2616db
arunsnd2017 <arunsinghsnd@gmail.com>
2020-05-04 05:28:27 +0530

    Updates

---
+++ precache-manifest.9ef3ecf1458ef7dca80ddff1c27fac92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5f314f0b57798f66954dd087da0694",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/index.html"
+  },
+  {
+    "revision": "d03b9d456e60bc117f39",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/css/main.2fe79772.chunk.css"
+  },
+  {
+    "revision": "5dfb52e6492cc18d519d",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/js/2.25ec9f42.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/js/2.25ec9f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03b9d456e60bc117f39",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/js/main.b4893027.chunk.js"
+  },
+  {
+    "revision": "8907d60bb2a18e0cc921",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/js/runtime-main.1a7aa855.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/arunsinghsnd/covid19-tracking-updates.git/static/media/image.d7265326.png"
+  }
+]);
